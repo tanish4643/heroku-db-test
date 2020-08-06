52d8f44ddf27094ea22ef2472b5a035b3ffd20d8
Tanish <tanish4643@yahoo.com>
2020-08-06 10:58:37 +0530

    crash fix 2

--- index.js
+++ index.js
@@ -71,4 +71,4 @@ app.get('/vegetable/:id', async (req, res) => {
 });
 /* ROUTE SECTION ENDS */
 
-app.listen(3000, () => console.log(`app listening on port 3000!`))
+app.listen(process.env.PORT || 3000, () => console.log(`app listening on port 3000!`))
